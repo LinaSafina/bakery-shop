80a9f51f35c36c9b9fcba0a93a56a0d821329751
Alina Safina <linasafina16@gmail.com>
2021-12-05 17:20:45 +0300

    Finish working on auth validation

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -9,15 +9,41 @@ const AuthForm = () => {
   const passwordInputRef = useRef();
   const [isLogin, setIsLogin] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [isEmailInvalid, setIsEmailInvalid] = useState(false);
+  const [isPasswordInvalid, setIsPasswordInvalid] = useState(false);
+  const [isFormTouched, setIsFormTouched] = useState(false);
   const history = useHistory();
+
+  //Validation
+  const validateInputEmail = (input) => {
+    return input.trim().length > 7 && input.includes('@');
+  };
+  const validateInputPassword = (input) => {
+    return input.trim().length > 5;
+  };
+
+  // let isEmailInvalid = false;
+  // let isPasswordInvalid = false;
 
   const submitHandler = (event) => {
     event.preventDefault();
     setIsLoading(true);
+    setIsFormTouched(true);
+
+    let isEmailValid = false;
+    let isPasswordValid = false;
+    let isFormValid = false;
+
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
-
-    // new
+    isEmailValid = validateInputEmail(enteredEmail);
+    isPasswordValid = validateInputPassword(enteredPassword);
+    let emailInvalid = !isEmailValid && isFormTouched;
+    setIsEmailInvalid(emailInvalid);
+    // isPasswordInvalid =
+    setIsPasswordInvalid(!isPasswordValid && isFormTouched);
+    isFormValid = isEmailValid && isPasswordValid;
+    console.log(isFormValid, isPasswordInvalid, isEmailInvalid);
 
     let requestUrl;
     if (isLogin) {
@@ -27,28 +53,32 @@ const AuthForm = () => {
       requestUrl =
         'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCFGHri0Xo-TPaq3aKL3N4uKuf3zzdsToc';
     }
-
-    fetch(requestUrl, {
-      method: 'POST',
-      body: JSON.stringify({
-        email: enteredEmail,
-        password: enteredPassword,
-        returnSecureToken: true,
-      }),
-      headers: { 'Content-type': 'pplication/json' },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Something went wrong');
-        }
-        return response.json();
+    // Fetch data
+    if (isFormValid) {
+      fetch(requestUrl, {
+        method: 'POST',
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: { 'Content-type': 'application/json' },
       })
-      .then((data) => {
-        setIsLoading(false);
-        authCtx.login(data.idToken);
-        history.push('/');
-      })
-      .catch((e) => alert(e));
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          if (data.error) {
+            throw new Error(data.error.message || 'Something went wrong');
+          }
+          // console.log(data);
+          authCtx.login(data.idToken);
+          history.push('/');
+        })
+        .catch((e) => alert(e))
+        .finally(() => setIsLoading(false));
+    }
+    setIsLoading(false);
   };
 
   const toggleButtonHandler = () => {
@@ -57,11 +87,13 @@ const AuthForm = () => {
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign up'}</h1>
-      <form onSubmit={submitHandler}>
+      <form onSubmit={submitHandler} noValidate>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
           <input type='email' id='email' required ref={emailInputRef} />
+          {isEmailInvalid && <span>Email is invalid</span>}
         </div>
+
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
           <input
@@ -70,6 +102,7 @@ const AuthForm = () => {
             required
             ref={passwordInputRef}
           />
+          {isPasswordInvalid && <span>Password is invalid</span>}
         </div>
         <div className={classes.actions}>
           {!isLoading && <button>{isLogin ? 'Login' : 'Sign up'}</button>}
